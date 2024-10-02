54c70a4b213ef3db7c0e91d7fef4228f79228324
Ileolami <99335756+Ileolami@users.noreply.github.com>
2024-10-02 11:19:32 +0100

    Update port to use hardcoded value

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 async function main() {
   const app = express();
-  const port = process.env.PORT || 3000;
+  const port = 3000;
 
   // Parse the request body as JSON
   app.use(bodyParser.json());
